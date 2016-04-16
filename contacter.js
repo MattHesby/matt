3cb8ae1cd66537894b13eaabed47ccf7ec22512d
Matt Hesby <matthesby@gmail.com>
2016-04-16 13:28:35 -0700

    wip

--- contacter.js
+++ contacter.js
@@ -4,9 +4,9 @@ var xoauth2 = require('xoauth2'), generator;
 
 generator = xoauth2.createXOAuth2Generator({
   user: "matthesby@gmail.com", // Your gmail address. // CHANGE THIS TO TBE //
-  clientId: "556514325539-j4rips0k8fl8h3gh4r8p3hnvdddcbcbi.apps.googleusercontent.com",// Your gmail address. // CHANGE THIS TO TBE //
-  clientSecret: "sFxOMX_ka-Tic-q_HobfG98E",// Your gmail address. // CHANGE THIS TO TBE //
-  refreshToken: "1/zFpdFoBF3iVoxXDVhZhyrBpEfAJmYDS9kpMAHPPtEbE",// Your gmail address. // CHANGE THIS TO TBE //
+  clientId: "clientID",// Your gmail address. // CHANGE THIS TO TBE //
+  clientSecret: "clientSecret",// Your gmail address. // CHANGE THIS TO TBE //
+  refreshToken: "REFRESHTOKENHERE",// Your gmail address. // CHANGE THIS TO TBE //
 });
 
 var transporter = nodemailer.createTransport({
